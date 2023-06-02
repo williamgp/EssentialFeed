0f1323ce4b11835d41ee8268dd8ce4d1339a14b2
William Peregoy <williamperegoy@gmail.com>
2023-06-01 22:20:59 -0700

    Added HTTPClient singleton and set the requestedURL when load is called

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -4,25 +4,29 @@ import XCTest
 
 class RemoteFeedLoader {
     func load() {
-        
+        HTTPClient.shared.requestedURL = URL(string: "http://a-url.com")
     }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    
+    private init() {}
+    
     var requestedURL: URL?
 }
 
 final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
                 
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestsDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         let sut = RemoteFeedLoader()
         
         sut.load()
