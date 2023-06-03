66824edf694fc9aa43c69e94736b173a75f685a4
William Peregoy <williamperegoy@gmail.com>
2023-06-02 17:14:48 -0700

    Close RemoteFeedLoader to subclassing

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -6,7 +6,7 @@ public protocol HTTPClient {
     func get(from url: URL)
 }
 
-public class RemoteFeedLoader {
+public final class RemoteFeedLoader {
     private let url: URL
     private let client: HTTPClient
     
