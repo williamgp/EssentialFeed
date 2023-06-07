4282e27269f67b06f37c910e21ce384f00ba9d9c
William Peregoy <williamperegoy@gmail.com>
2023-06-06 22:02:09 -0700

    Remove unused line (copy/paste leftover)

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -48,7 +48,6 @@ final class RemoteFeedLoaderTests: XCTestCase {
         var capturedErrors = [RemoteFeedLoader.Error?]()
         sut.load() { capturedErrors.append($0) }
         
-        let clientError = NSError(domain: "Test", code: 0)
         let statusCodes = [199, 201, 300, 400, 500]
         statusCodes.forEach { code in
             client.complete(withStatusCode: code)
