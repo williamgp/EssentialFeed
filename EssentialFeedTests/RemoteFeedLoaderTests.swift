4bd9e592081ad2fb9a4fd97d6d0f10dc44b429eb
William Peregoy <williamperegoy@gmail.com>
2023-06-07 16:27:46 -0700

    Use helper method in our new test as well

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -63,13 +63,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversNoItemsOn200AndEmptyJSON() {
         let (sut, client) = makeSUT()
         
-        var capturedResults = [RemoteFeedLoader.Result?]()
-        sut.load() { capturedResults.append($0) }
-        
-        let emptyListJSON = Data("{\"items\": []}".utf8)
-        client.complete(withStatusCode: 200, data: emptyListJSON)
-        
-        XCTAssertEqual(capturedResults, [.success([])])
+        expect(sut, toCompleteWith: .success([])) {
+            let emptyListJSON = Data("{\"items\": []}".utf8)
+            client.complete(withStatusCode: 200, data: emptyListJSON)
+        }
     }
     
     // MARK: - Helpers
