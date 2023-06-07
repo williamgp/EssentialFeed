90fd02979b2e51d08693f06c9497a563d9811f98
William Peregoy <williamperegoy@gmail.com>
2023-06-06 17:07:17 -0700

    Capture the completions in the spy rather than stubbing

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -32,11 +32,13 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0)
         
         var capturedErrors = [RemoteFeedLoader.Error?]()
         sut.load() { capturedErrors.append($0) }
         
+        let clientError = NSError(domain: "Test", code: 0)
+        client.completions.first!(clientError)
+
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
     
@@ -51,12 +53,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
-        var error: Error?
+        var completions = [(Error) -> Void]()
         
         func get(from url: URL, completion: @escaping (Error) -> Void) {
-            if let error = error {
-                completion(error)
-            }
+            completions.append(completion)
             requestedURLs.append(url)
 
         }
