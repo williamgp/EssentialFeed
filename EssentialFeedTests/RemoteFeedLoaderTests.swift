6056c79a1e77efbc425433982b0684e7da5128f2
William Peregoy <williamperegoy@gmail.com>
2023-06-06 17:17:59 -0700

    Remove default completion handler from load() function

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -15,7 +15,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(string: "http://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.load()
+        sut.load() { _ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
     }
@@ -24,8 +24,8 @@ final class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(string: "http://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.load()
-        sut.load()
+        sut.load() { _ in }
+        sut.load() { _ in }
 
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
