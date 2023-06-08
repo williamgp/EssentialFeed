fb29076d615888f7ea7c81d6f3784b45bc6a7c38
William Peregoy <williamperegoy@gmail.com>
2023-06-07 17:44:53 -0700

    Hide our API models inside our Mapper function where they are used

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -44,6 +44,24 @@ public final class RemoteFeedLoader {
 }
 
 private class FeedItemsMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
+
+    private struct Item: Identifiable, Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
+        
+        var item: FeedItem {
+            FeedItem(id: id,
+                     description: description,
+                     location: location,
+                     imageURL: image)
+        }
+    }
+
     static func map(_ data: Data, response: HTTPURLResponse) throws -> [FeedItem] {
         guard response.statusCode == 200 else {
             throw RemoteFeedLoader.Error.invalidData
@@ -53,20 +71,3 @@ private class FeedItemsMapper {
     }
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
-
-private struct Item: Identifiable, Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
-    
-    var item: FeedItem {
-        FeedItem(id: id,
-                 description: description,
-                 location: location,
-                 imageURL: image)
-    }
-}
