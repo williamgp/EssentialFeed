a21d5cd5cd11db20898e5e4d30940e16815f97ac
William Peregoy <williamperegoy@gmail.com>
2023-06-09 16:42:12 -0700

    Simplify our mapping method, using the guard let

--- EssentialFeed/Feed API/FeedItemsMapper.swift
+++ EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -24,17 +24,13 @@ internal final class FeedItemsMapper {
     }
     
     internal static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteFeedLoader.Result {
-        guard response.statusCode == OK_200 else {
+        guard response.statusCode == OK_200,
+              let root = try? JSONDecoder().decode(Root.self, from: data) else {
             return .failure(.invalidData)
         }
         
-        do {
-            let root = try JSONDecoder().decode(Root.self, from: data)
-            let items = root.items.map { $0.item }
-            return .success(items)
-        } catch {
-            return .failure(.invalidData)
-        }
+        let items = root.items.map { $0.item }
+        return .success(items)
     }
 }
 
