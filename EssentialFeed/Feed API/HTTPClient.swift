ab444545d40a6cd03293a74f17f47fb3c0e6a497
William Peregoy <williamperegoy@gmail.com>
2023-06-09 16:05:40 -0700

    Some organization: HTTPClient can move to its own file

---
+++ EssentialFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,10 @@
+//  Created by Wiiliam Peregoy on 6/9/23
+
+import Foundation
+
+public typealias HTTPClientResult = Result <(Data, HTTPURLResponse), Error>
+
+public protocol HTTPClient {
+    func get(from url: URL,
+             completion: @escaping (HTTPClientResult) -> Void)
+}
