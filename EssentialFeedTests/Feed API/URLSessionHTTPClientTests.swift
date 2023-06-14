f62ddeee954bcdf1cc3a3d875fa290baa53b9425
William Peregoy <williamperegoy@gmail.com>
2023-06-14 16:41:36 -0700

    Move the URLSessionHTTPClient (or SUT) to a factory method

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -41,7 +41,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
        
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         
         wait(for: [exp], timeout: 1.0)
     }
@@ -51,11 +51,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "any error", code: 1)
     
         URLProtocolStub.stub(url: url, data: nil, response: nil, error: error)
-        
-        let sut = URLSessionHTTPClient()
-        
+                
         let exp = expectation(description: "Wait for completion.")
-        sut.get(from: url) { result in
+        
+        makeSUT().get(from: url) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -67,10 +66,14 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
         wait(for: [exp], timeout: 1.0)
     }
+    
+    // MARK: - Helpers
+
+    private func makeSUT() -> URLSessionHTTPClient {
+        return URLSessionHTTPClient()
+    }
 }
 
-// MARK: - Helpers
- 
 private class URLProtocolStub: URLProtocol {
     private static var stub: Stub?
     private static var requestObserver: ((URLRequest) -> Void)?
@@ -80,7 +83,7 @@ private class URLProtocolStub: URLProtocol {
         let data: Data?
         let response: URLResponse?
     }
-    
+        
     static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?) {
         stub = Stub(error: error, data: data, response: response)
     }
