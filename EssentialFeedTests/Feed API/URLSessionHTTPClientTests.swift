4d9778dc570bfba1d30963539636e4ad2baa9385
William Peregoy <williamperegoy@gmail.com>
2023-06-14 17:15:51 -0700

    Delivers error on all invalid (or unexpected) representation cases

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -59,8 +59,22 @@ final class URLSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(receivedError?.code, requestError.code)
     }
     
-    func test_getFromURL_failsOnAllNilValues() {
+    func test_getFromURL_failsOnAllInvalidRepresentationCases() {
+        let anyData = Data("any data".utf8)
+        let anyError = NSError(domain: "any error", code: 1)
+        let nonHTTPURLResponse = URLResponse(url: anyURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+        let anyHTTPURLResponse = HTTPURLResponse(url: anyURL(), statusCode: 200, httpVersion: nil, headerFields: nil)
+        
         XCTAssertNotNil(resultError(data: nil, response: nil, error: nil))
+        XCTAssertNotNil(resultError(data: nil, response: nonHTTPURLResponse, error: nil))
+        XCTAssertNotNil(resultError(data: nil, response: anyHTTPURLResponse, error: nil))
+        XCTAssertNotNil(resultError(data: anyData, response: nil, error: nil))
+        XCTAssertNotNil(resultError(data: anyData, response: nil, error: anyError))
+        XCTAssertNotNil(resultError(data: nil, response: nonHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultError(data: nil, response: anyHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultError(data: anyData, response: nonHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultError(data: anyData, response: anyHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultError(data: anyData, response: nonHTTPURLResponse, error: nil))
     }
     
     // MARK: - Helpers
