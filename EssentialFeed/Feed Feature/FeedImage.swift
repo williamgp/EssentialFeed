aff2afb4ba308bf72fbadb54cf497ef3397f8f41
William Peregoy <williamperegoy@gmail.com>
2023-07-27 12:36:11 -0700

    remove Identifiable conformance (its just not needed at this point)

--- EssentialFeed/Feed Feature/FeedImage.swift
+++ EssentialFeed/Feed Feature/FeedImage.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 
-public struct FeedImage: Identifiable, Equatable {
+public struct FeedImage: Equatable {
     public let id: UUID
     public let description: String?
     public let location: String?
