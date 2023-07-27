7fc21cd24fd1b98461def783da26a4535ff0aa34
William Peregoy <williamperegoy@gmail.com>
2023-07-27 13:36:18 -0700

    remove Identifiable conformance (its just not needed at this point) (#21)

--- EssentialFeed/Feed API/RemoteFeedItem.swift
+++ EssentialFeed/Feed API/RemoteFeedItem.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 
-internal struct RemoteFeedItem: Identifiable, Decodable {
+internal struct RemoteFeedItem: Decodable {
     internal let id: UUID
     internal let description: String?
     internal let location: String?
--- EssentialFeed/Feed Cache/LocalFeedImage.swift
+++ EssentialFeed/Feed Cache/LocalFeedImage.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 
-public struct LocalFeedImage: Identifiable, Equatable {
+public struct LocalFeedImage: Equatable {
     public let id: UUID
     public let description: String?
     public let location: String?
