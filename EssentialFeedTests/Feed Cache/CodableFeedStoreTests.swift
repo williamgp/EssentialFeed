b4e2d138de27e67b2b0cefd143f937808b6dd7fd
William Peregoy <williamperegoy@gmail.com>
2023-07-27 11:43:52 -0700

    Retrieving from empty cache twice delivers the same empty result (no side-effects)

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -7,7 +7,6 @@ class CodableFeedStore {
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
         completion(.empty)
     }
-
 }
 
 final class CodableFeedStoreTests: XCTestCase {
@@ -30,5 +29,27 @@ final class CodableFeedStoreTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
     }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let exp = expectation(description: "Wait for cache retrieval")
+        
+        sut.retrieve { firstResult in
+            sut.retrieve { secondResult in
+                switch (firstResult, secondResult) {
+                case (.empty, .empty):
+                    break
+                    
+                default:
+                    XCTFail("Expected retrieving twice from empty cache to deliver same empty result, got \(firstResult) and \(secondResult) instead")
+                }
+                
+                exp.fulfill()
+            }
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     
 }
--- README.md
+++ README.md
@@ -130,10 +130,13 @@ Given the customer doesn't have connectivity
 
 ### Local feed cache triangulation
 - Retrieve
-    - Empty cache
+    ✅ Empty cache returns empty
+    - Empty cache twice returns empty (no side-effects)
     - Non-empty cache returns data
     - Non-empty cache twice returns same data (no side-effects)
-    - Error (if applicable, eg. invalid data)
+    - Error returns error (if applicable, eg. invalid data)
+    - Error twice returns same error (if applicable, eg. invalid data)
+
 
 - Insert
     - To empty cache stores data
