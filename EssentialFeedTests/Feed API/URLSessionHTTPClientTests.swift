b994ae3e15960c24a7598a1168b6dd55ba698add
William Peregoy <williamperegoy@gmail.com>
2023-09-07 22:10:58 -0700

    Rescoped HTTPClientResult to HTTPClient.Result

--- EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -108,13 +108,13 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
 
     private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #filePath,
-                            line: UInt = #line) -> HTTPClientResult {
+                           line: UInt = #line) -> HTTPClient.Result {
       
         URLProtocolStub.stub(data: data, response: response, error: error)
         let sut = makeSUT(file: file, line: line)
         
         let exp = expectation(description: "Wait for completion.")
-        var receivedResult: HTTPClientResult?
+        var receivedResult: HTTPClient.Result?
         
         sut.get(from: anyURL()) { result in
             receivedResult = result
