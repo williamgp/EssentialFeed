3c9094898a7c032ac400ab3698cc4e03e9e52d6a
William Peregoy <williamperegoy@gmail.com>
2023-09-21 09:55:02 -0700

    explicit self in closure

--- EssentialFeediOS/FeedViewController.swift
+++ EssentialFeediOS/FeedViewController.swift
@@ -64,7 +64,7 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
         let loadImage = { [weak self, weak cell] in
             guard let self = self else { return }
             
-            self.tasks[indexPath] = imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
+            self.tasks[indexPath] = self.imageLoader?.loadImageData(from: cellModel.url) { [weak cell] result in
                 let data = try? result.get()
                 let image = data.map(UIImage.init) ?? nil
                 cell?.feedImageView.image = image
