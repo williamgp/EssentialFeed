7e9e0d9bcb3b0735dbbefea008afc24aaa60d5a7
William Peregoy <williamperegoy@gmail.com>
2024-01-29 17:09:00 -0800

    minor refactor & cleanup of tests

---
+++ EssentialFeediOSTests/Feed UI/Controllers/Helpers/FakeRefreshControl.swift
@@ -0,0 +1,17 @@
+//  Created by William Peregoy on 1/29/24
+
+import UIKit
+
+class FakeRefreshControl: UIRefreshControl {
+    private var _isRefreshing = false
+    
+    override var isRefreshing: Bool { _isRefreshing }
+    
+    override func beginRefreshing() {
+        _isRefreshing = true
+    }
+    
+    override func endRefreshing() {
+        _isRefreshing = false
+    }
+}
