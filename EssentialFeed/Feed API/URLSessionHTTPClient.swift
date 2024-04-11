9d33c2d8e80561c105cf30363072f457c6fdef7b
William Peregoy <williamperegoy@gmail.com>
2024-04-11 12:59:39 +0800

    Add HTTPClientTask so clients can cancel HTTP requests

--- EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -2,7 +2,7 @@
 
 import Foundation
 
-public class URLSessionHTTPClient: HTTPClient {
+public final class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
     
     public init(session: URLSession = .shared) {
@@ -11,18 +11,27 @@ public class URLSessionHTTPClient: HTTPClient {
     
     private struct UnexpectedValuesRepresentation: Error {}
     
-    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-        session.dataTask(with: url) { data, response, error in
-            completion(Result {
-                if let error = error {
-                    throw error
-                } else if let data = data,
-                          let response = response as? HTTPURLResponse {
-                    return (data, response)
-                } else {
-                    throw UnexpectedValuesRepresentation()
-                }
-            })
-        }.resume()
+    private struct URLSessionTaskWrapper: HTTPClientTask {
+        let wrapped: URLSessionTask
+        
+        func cancel() {
+            wrapped.cancel()
+        }
+    }
+    
+    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+        let task = session.dataTask(with: url) { data, response, error in completion(Result {
+            if let error = error {
+                throw error
+            } else if let data = data,
+                      let response = response as? HTTPURLResponse {
+                return (data, response)
+            } else {
+                throw UnexpectedValuesRepresentation()
+            }
+        })
+        }
+        task.resume()
+        return URLSessionTaskWrapper(wrapped: task)
     }
 }
