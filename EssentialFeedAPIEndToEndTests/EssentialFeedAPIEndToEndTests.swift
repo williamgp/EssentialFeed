67a01b51fdec8a1d64cefa39de8976bb18d2b77d
William Peregoy <williamperegoy@gmail.com>
2024-04-23 12:36:37 +0800

    Extract duplicate base URL creation into a factory helper

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -42,9 +42,8 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
     // MARK: - Helpers
     
     private func getFeedResult(file: StaticString = #filePath, line: UInt = #line) -> FeedLoader.Result? {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        let loader = RemoteFeedLoader(url: feedTestServerURL, client: client)
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
 
@@ -62,7 +61,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
     }
     
     private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
-        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")!
+        let url = feedTestServerURL.appendingPathComponent("73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")
         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedImageDataLoader(client: client)
         trackForMemoryLeaks(client, file: file, line: line)
@@ -71,7 +70,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
         let exp = expectation(description: "Wait for load completion")
         
         var receivedResult: FeedImageDataLoader.Result?
-        _ = loader.loadImageData(from: testServerURL) { result in
+        _ = loader.loadImageData(from: url) { result in
             receivedResult = result
             exp.fulfill()
         }
@@ -79,7 +78,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
         
         return receivedResult
     }
-
+    
     private func expectedImage(at index: Int) -> FeedImage {
         return FeedImage(
             id: id(at: index),
@@ -90,45 +89,50 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
     }
     
     private func id(at index: Int) -> UUID {
-            return UUID(uuidString: [
-                "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
-                "BA298A85-6275-48D3-8315-9C8F7C1CD109",
-                "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
-                "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
-                "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
-                "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
-                "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
-                "F79BD7F8-063F-46E2-8147-A67635C3BB01"
-            ][index])!
-        }
-        
-        private func description(at index: Int) -> String? {
-            return [
-                "Description 1",
-                nil,
-                "Description 3",
-                nil,
-                "Description 5",
-                "Description 6",
-                "Description 7",
-                "Description 8"
-            ][index]
-        }
-        
-        private func location(at index: Int) -> String? {
-            return [
-                "Location 1",
-                "Location 2",
-                nil,
-                nil,
-                "Location 5",
-                "Location 6",
-                "Location 7",
-                "Location 8"
-            ][index]
-        }
-        
-        private func imageURL(at index: Int) -> URL {
-            return URL(string: "https://url-\(index+1).com")!
-        }
+        return UUID(uuidString: [
+            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+            "F79BD7F8-063F-46E2-8147-A67635C3BB01"
+        ][index])!
+    }
+    
+    private func description(at index: Int) -> String? {
+        return [
+            "Description 1",
+            nil,
+            "Description 3",
+            nil,
+            "Description 5",
+            "Description 6",
+            "Description 7",
+            "Description 8"
+        ][index]
+    }
+    
+    private func location(at index: Int) -> String? {
+        return [
+            "Location 1",
+            "Location 2",
+            nil,
+            nil,
+            "Location 5",
+            "Location 6",
+            "Location 7",
+            "Location 8"
+        ][index]
+    }
+    
+    private func imageURL(at index: Int) -> URL {
+        return URL(string: "https://url-\(index+1).com")!
+    }
+    
+    
+    private var feedTestServerURL: URL {
+        return URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+    }
 }
