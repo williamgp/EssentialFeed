10a83ae73f7d2fa7c195948305453dc30bfccb23
William Peregoy <williamperegoy@gmail.com>
2024-08-06 12:15:11 -0700

    `LocalFeedLoader.validateCache` fails on deletion error of a failed retrieval

--- EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -67,7 +67,7 @@ extension LocalFeedLoader {
             guard let self = self else { return }
             switch result {
             case .failure:
-                self.store.deleteCachedFeed { _ in completion(.success(())) }
+                self.store.deleteCachedFeed(completion: completion)
 
             case let .success(.some((feed: _, timestamp: timestamp))) where !FeedCachePolicy.validate(timestamp, against: self.currentDate()):
                 self.store.deleteCachedFeed { _ in completion(.success(())) }
