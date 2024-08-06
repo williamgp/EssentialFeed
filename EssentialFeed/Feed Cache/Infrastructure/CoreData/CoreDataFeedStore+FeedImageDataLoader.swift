c55d36da1446d9831f298f8874a21ae2e79dd3b9
William Peregoy <williamperegoy@gmail.com>
2024-08-06 11:26:30 -0700

    Move `CoreDataFeedStore` implementation of the `FeedImageDataStore` protocol to production

---
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -0,0 +1,15 @@
+//  Created by Wiiliam Peregoy on 8/6/24
+
+import Foundation
+
+extension CoreDataFeedStore: FeedImageDataStore {
+    
+    public func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        
+    }
+    
+    public func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        completion(.success(.none))
+    }
+    
+}
