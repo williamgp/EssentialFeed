2061503b2638849b45897b3a7b1760b8dcbcbe43
William Peregoy <williamperegoy@gmail.com>
2024-08-06 14:14:38 -0700

    EssentialApp project & workspace

---
+++ EssentialApp/EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,14 @@
+//  Created by Wiiliam Peregoy on 8/6/24
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
