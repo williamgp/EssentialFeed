81ed8a532dead7d0ca2130654dca72e5ed8d6f33
William Peregoy <williamperegoy@gmail.com>
2024-08-06 11:50:11 -0700

    Encapsulate `CoreDataStore` bundle creation into a centralized place to remove duplication and prevent mistakes

--- EssentialFeedTests/Feed Cache/CoreDateFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDateFeedStoreTests.swift
@@ -79,9 +79,8 @@ final class CoreDateFeedStoreTests: XCTestCase, FeedStoreSpecs {
     // - MARK: Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> FeedStore {
-        let storeBundle = Bundle(for: CoreDataFeedStore.self)
         let storeURL = URL(fileURLWithPath: "/dev/null")
-        let sut = try! CoreDataFeedStore(storeURL: storeURL, bundle: storeBundle)
+        let sut = try! CoreDataFeedStore(storeURL: storeURL)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
