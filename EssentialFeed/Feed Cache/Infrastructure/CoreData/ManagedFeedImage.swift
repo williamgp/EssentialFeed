db51a63eec69b431e8f9fffac75d7855b701d0cb
William Peregoy <williamperegoy@gmail.com>
2024-08-06 11:29:16 -0700

    `CoreDataFeedStore.retrieveImageData` delivers stored data when there's an image with a matching URL in the store

--- EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedFeedImage.swift
@@ -13,18 +13,26 @@ class ManagedFeedImage: NSManagedObject {
 }
 
 extension ManagedFeedImage {
-     static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
-         return NSOrderedSet(array: localFeed.map { local in
-             let managed = ManagedFeedImage(context: context)
-             managed.id = local.id
-             managed.imageDescription = local.description
-             managed.location = local.location
-             managed.url = local.url
-             return managed
-         })
-     }
-     
-     var local: LocalFeedImage {
-         return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
-     }
- }
+    static func first(with url: URL, in context: NSManagedObjectContext) throws -> ManagedFeedImage? {
+        let request = NSFetchRequest<ManagedFeedImage>(entityName: entity().name!)
+        request.predicate = NSPredicate(format: "%K = %@", argumentArray: [#keyPath(ManagedFeedImage.url), url])
+        request.returnsObjectsAsFaults = false
+        request.fetchLimit = 1
+        return try context.fetch(request).first
+    }
+    
+    static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+        return NSOrderedSet(array: localFeed.map { local in
+            let managed = ManagedFeedImage(context: context)
+            managed.id = local.id
+            managed.imageDescription = local.description
+            managed.location = local.location
+            managed.url = local.url
+            return managed
+        })
+    }
+    
+    var local: LocalFeedImage {
+        return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
+    }
+}
