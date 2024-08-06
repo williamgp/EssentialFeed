a4ec3fa8a51492608ed9f0eabee934845a7eadf1
William Peregoy <williamperegoy@gmail.com>
2024-08-06 10:21:10 -0700

    Move `LocalFeedImageDataLoader` and `FeedImageDataStore` to production

---
+++ EssentialFeed/Feed Cache/FeedImageDataStore.swift
@@ -0,0 +1,9 @@
+//  Created by William Peregoy on 8/6/24
+
+import Foundation
+
+public protocol FeedImageDataStore {
+    typealias Result = Swift.Result<Data?, Error>
+    
+    func retrieve(dataForURL url: URL, completion: @escaping (Result) -> Void)
+}
