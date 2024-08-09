2f57af440de4a2bb116bf75de0be9ee3fb3b2c92
William Peregoy <williamperegoy@gmail.com>
2024-08-08 17:27:45 -0700

    Move `FeedLoaderCacheDecorator` to production

---
+++ EssentialApp/EssentialApp/EssentialApp/FeedLoaderCacheDecorator.swift
@@ -0,0 +1,23 @@
+//  Created by William Peregoy on 8/8/24
+
+import Foundation
+import EssentialFeed
+
+final public class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    private let cache: FeedCache
+    
+    public init(decoratee: FeedLoader, cache: FeedCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            completion(result.map { feed in
+                self?.cache.save(feed) { _ in }
+                return feed
+            })
+        }
+    }
+}
