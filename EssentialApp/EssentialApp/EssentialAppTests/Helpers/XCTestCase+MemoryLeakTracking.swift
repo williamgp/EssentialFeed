6a560077f245082960afb58b3b3d49be9d28c5f6
William Peregoy <williamperegoy@gmail.com>
2026-04-11 11:02:30 -0700

    Fix project

---
+++ EssentialApp/EssentialApp/EssentialAppTests/Helpers/XCTestCase+MemoryLeakTracking.swift
@@ -0,0 +1,13 @@
+//  Created by William Peregoy on 6/14/23
+
+import XCTest
+
+extension XCTestCase {
+    func trackForMemoryLeaks(_ instance: AnyObject,
+                             file: StaticString = #filePath,
+                             line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
